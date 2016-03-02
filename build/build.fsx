f0d3431ce3ec108b3de32fea2fc78dd5a1e9fe34
Adrian Macal <adrian.macal@check24.de>
2016-03-02 13:05:17 +0100

    Relase 2.2

--- build/build.fsx
+++ build/build.fsx
@@ -39,7 +39,7 @@ Target "ExecuteTests" (fun _ ->
 Target "CreatePackage" (fun _ ->
      NuGet (fun p -> 
         { p with
-            Version = "2.1"
+            Version = "2.2"
             OutputPath = "./build/package"
             WorkingDir = "./build/release"
             Files = [( "Omlet.dll", Some "lib\\net45", None )]
